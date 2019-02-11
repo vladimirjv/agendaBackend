30272dabb007e03202b353af492a018e4140d502
vladimirjv <ivladimirjuarez@outlook.com>
2019-02-11 16:22:34 -0600

    agregando soporte CORS

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
-var db = mongoose.connect('mongodb://vladimirjv:cetmar07059669@ds331135.mlab.com:31135/heroku_h34kk55n');
-// mongoose.connect('mongodb://vladimirjv:cetmar07059669@ds331135.mlab.com:31135/heroku_h34kk55n');
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//     console.log('Entramos!');
-// });
-
-var Contact = require('./Models/contactModel');
-
 var port = process.env.PORT || 3000;
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// var contactRouter = require('./Routes/contacts.routes')();
+var db = mongoose.connect('mongodb://vladimirjv:cetmar07059669@ds331135.mlab.com:31135/heroku_h34kk55n');
+var Contact = require('./Models/contactModel');
+
 var contactRouter = require('./Routes/contacts.routes')(Contact);
 app.use('/api/contacts', contactRouter);
 
