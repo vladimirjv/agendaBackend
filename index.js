12965e31a97638665930417d2a434f864687dec3
vladimirjv <ivladimirjuarez@outlook.com>
2019-02-11 18:28:46 -0600

    mongo uri

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://vladimirjv:cetmar07059669@ds331135.mlab.com:31135/heroku_h34kk55n');
+// var db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://vladimirjv:cetmar07059669@ds331135.mlab.com:31135/heroku_h34kk55n');
+var db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_h34kk55n:io2oaau81lkvivhhb9nfcptn5r@ds331135.mlab.com:31135/heroku_h34kk55n');
 var Contact = require('./Models/contactModel');
 
 var contactRouter = require('./Routes/contacts.routes')(Contact);
