852833a11c2ba25d1ef465d01be6bce8acd1987f
vladimirjv <ivladimirjuarez@outlook.com>
2019-02-12 07:05:24 -0600

    comentarios sobre script de servidor

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+//serve instance
 var app = express();
-
+//serve port
 var port = process.env.PORT || 3000;
+//middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+// mongoose connection
 var db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://vladimirjv:cetmar07059669@ds331135.mlab.com:31135/heroku_h34kk55n');
 var Contact = require('./Models/contactModel');
-
+// router
 var contactRouter = require('./Routes/contacts.routes')(Contact);
 app.use('/api/contacts', contactRouter);
 
@@ -21,6 +23,6 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('App listening on port '+port);
 });
 
